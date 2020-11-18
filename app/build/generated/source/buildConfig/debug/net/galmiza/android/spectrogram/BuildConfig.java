31aa37b5c4a6649104bad10f8af24750c14a7bc7
lootiangkuan <888-lootiangkuan@users.noreply.git.sysop.bigo.sg>
2020-11-18 18:58:41 +0800

    Initial Commit

---
+++ app/build/generated/source/buildConfig/debug/net/galmiza/android/spectrogram/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.galmiza.android.spectrogram;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.galmiza.android.spectrogram";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
